e74c5bfca6e681c6e756085f463701665d7fa5fd
bds248a <bds24.8a@gmail.com>
2026-01-15 09:28:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 st.title("Любими неща – класна анкета")
 
+# Инициализация на session_state
 if "colors" not in st.session_state:
     st.session_state.colors = {
         "Червен": 0,
@@ -21,5 +22,29 @@ if "sports" not in st.session_state:
 
 st.subheader("Избери любими неща")
 
+# Избори
 color = st.selectbox("Любим цвят:", list(st.session_state.colors.keys()))
-sport = st.selectbox("Любим спорт:", list(st.session_state._
+sport = st.selectbox("Любим спорт:", list(st.session_state.sports.keys()))
+
+# Бутон за запис
+if st.button("Запази избора"):
+    st.session_state.colors[color] += 1
+    st.session_state.sports[sport] += 1
+    st.success("Изборът е записан!")
+
+st.divider()
+st.subheader("Резултати")
+
+# Графика за цветовете
+st.write("Любими цветове")
+colors_df = pd.DataFrame.from_dict(
+    st.session_state.colors, orient="index", columns=["Брой"]
+)
+st.bar_chart(colors_df)
+
+# Графика за спортовете
+st.write("Любими спортове")
+sports_df = pd.DataFrame.from_dict(
+    st.session_state.sports, orient="index", columns=["Брой"]
+)
+st.bar_chart(sports_df)
