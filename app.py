e441197fbe3701603b7da28c715aa8e2ad58d5ee
bds248a <bds24.8a@gmail.com>
2026-01-15 09:42:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.title("Ученици и оценки")
 
-# Инициализация на session_state
+
 if "students" not in st.session_state:
     st.session_state.students = {
         "Петър": 0,
@@ -23,11 +23,10 @@ if "grades" not in st.session_state:
 
 st.subheader("Избери любими неща")
 
-# Избори
+
 students = st.selectbox("Ученик:", list(st.session_state.students.keys()))
 grades = st.selectbox("Оценка:", list(st.session_state.grades.keys()))
 
-# Бутон за запис
 if st.button("Запази избора"):
     st.session_state.students[students] += 1
     st.session_state.grades[grades] += 1
@@ -36,16 +35,16 @@ if st.button("Запази избора"):
 st.divider()
 st.subheader("Резултати")
 
-# Графика за цветовете
+
 st.write("Ученик")
 students_df = pd.DataFrame.from_dict(
     st.session_state.students, orient="index", columns=["Брой"]
 )
 st.bar_chart(students_df)
 
-# Графика за спортовете
+
 st.write("Оценка")
-sports_df = pd.DataFrame.from_dict(
+grades_df = pd.DataFrame.from_dict(
     st.session_state.grades, orient="index", columns=["Брой"]
 )
 st.bar_chart(grades_df)
